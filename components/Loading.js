63cd00c4b0e7bc72a53a7ad3c983d7c79972f79d
shreya-nipanikar <nipanikar.shreya1@gmail.com>
2021-07-12 20:09:45 -0400

    entire

--- components/Loading.js
+++ components/Loading.js
@@ -1,15 +1,15 @@
 import { Circle } from "better-react-spinkit";
-import Image from "next/image";
+
 function Loading() {
   return (
     <center style={{ display: "grid", placeItems: "center", height: "100vh" }}>
       <div>
-        <Image
+        <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/479px-WhatsApp.svg.png"
           alt=""
           style={{ marginBottom: 10 }}
           height={200}
-        />
+        ></img>
         <Circle color="#3CBC28" size={60} />
       </div>
     </center>
